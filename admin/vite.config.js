6b4f193ec510e507a396f41f17b641af4821ca25
Stefaan <safwbdev@gmail.com>
2025-03-26 16:17:42 +0800

    update vite config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8800",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = `${env.VITE_API_URL ?? 'http://localhost:8800'}`;
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
+        }
       }
     }
   }
